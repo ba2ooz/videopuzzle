513c9d200abe9b3afd3562dcf7edd52e4563ebfb
DA <dragan.anatolie.m@gmail.com>
2025-07-10 12:46:45 +0300

    add prod endpoint

--- frontend/src/services/db/PocketBaseClient.js
+++ frontend/src/services/db/PocketBaseClient.js
@@ -1,5 +1,10 @@
 import PocketBase from 'pocketbase'
 
-const pb = new PocketBase("http://127.0.0.1:8090");
+const isLocal = window.location.hostname === 'localhost';
+const API_BASE = isLocal
+  ? 'http://127.0.0.1:8090'
+  : 'https://videopuzzle-production.up.railway.app';
+
+const pb = new PocketBase(API_BASE);
 
 export default pb;
